ff8b570a169faa41ac56588cb45a2bd5df108aea
Adam Petráš <abushrek@seznam.cz>
2021-01-30 19:20:05 +0100

    Remove unused references

--- SimpleChat.Dal/Repositories/UserRepository.cs
+++ SimpleChat.Dal/Repositories/UserRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using SimpleChat.DAL.Entities;
 using SimpleChat.DAL.Repositories.Interfaces;
 
